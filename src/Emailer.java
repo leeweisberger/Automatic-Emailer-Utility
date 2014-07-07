5f08f97d73270d7d90ca988e3e3ba565d7c13fcd
Weisberger <lw033589@mac-lw033589.cerner.com>
2014-07-07 09:22:03 -0500

    created automatic emailer utility class

---
+++ src/Emailer.java
@@ -0,0 +1,50 @@
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class Emailer {
+    public static void sendEmail(final String from, final String password, final String to,
+            String subject, String body) {
+        String host = "smtp.gmail.com";
+
+        Properties properties = System.getProperties();
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", host);
+        properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.auth", "true");
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
+        
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(
+                    to));
+
+            message.setSubject(subject);
+            message.setText(body);
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Emailer.sendEmail("lweisberger5@gmail.com", "em@!lem@!l",
+                "lweisberger5@gmail.com", "This is the subject",
+                "This is the body");
+    }
+}
